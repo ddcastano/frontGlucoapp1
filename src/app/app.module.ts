ae8e82163acc35505e85476b3a84f54dea15df9f
ddcastano <dadaniel2001@gmail.com>
2023-05-29 19:27:30 -0500

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,18 +17,11 @@ import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatAutocompleteModule} from '@angular/material/autocomplete';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 89ba7f7a0ffac3c4cc6edd841d297a398eb54954
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { JsonPipe, NgIf } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-
-
 
 import { MatButtonModule } from '@angular/material/button';
 
@@ -60,12 +53,9 @@ import { MatButtonModule } from '@angular/material/button';
     NgIf,
     JsonPipe,
     MatIconModule,
-<<<<<<< HEAD
-    MatCardModule
-=======
+    MatCardModule,
     MatButtonModule,
     
->>>>>>> 89ba7f7a0ffac3c4cc6edd841d297a398eb54954
   ],
   providers: [Router],
   bootstrap: [AppComponent]
